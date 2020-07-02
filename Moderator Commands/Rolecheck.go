a64bfea3790ad8c8ddf97a971f01a79c7ec1617d
Bwalston <36297325+LtLolFace@users.noreply.github.com>
2020-07-01 17:52:53 -0700

    Update Rolecheck.go

--- Moderator Commands/Rolecheck.go
+++ Moderator Commands/Rolecheck.go
@@ -1,7 +1,7 @@
 {{$x := (userArg .User.ID).Mention}}
 {{if targetHasRoleID $x RoleNumID}}
 /* Determines users ID and uses it in this command */
-/* Find your RoleNumID by using command "listroles". */
+/* Find your RoleNumID by using command "listroles" */
 Rolecheck passed!
 /* Insert custom text here. */
 {{exec "role" "RoleName"}}
